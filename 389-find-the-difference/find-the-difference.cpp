37a0e46d4265001dc3dedf0c4bf1092c005e0912
Ritu Kumar <ritu748841@gmail.com>
2025-12-29 22:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (76.33%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum_s = 0;
+        for (char &ch : s) {
+            sum_s += ch;
+        }
+
+        int sum_t = 0;
+        for (char &ch : t) {
+            sum_t += ch;
+        }
+        return (char)(sum_t - sum_s);
+    }
+};
