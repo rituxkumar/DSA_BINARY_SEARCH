22bbbe09436c881252a3f771e4ccbec8881adbca
Ritu Kumar <ritu748841@gmail.com>
2025-12-30 21:51:53 +0530

    Time: 12 ms (49.77%) | Memory: 94.5 MB (31.42%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>>result(m,vector<int>(n));
+         if(original.size() != m*n){
+            return {};
+         }
+
+         for(int i =0;i<original.size();i++){
+            int row = i/n;
+            int column = i%n;
+
+            result[row][column] = original[i];
+         }
+
+         return result;
+
+    }
+};
